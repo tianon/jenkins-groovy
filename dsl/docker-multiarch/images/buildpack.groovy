3c75cc9e4d2f193ba69e750a8a05f34df86d9e88
Tianon Gravi <admwiggin@gmail.com>
2015-12-23 09:28:40 -0800

    Add initial buildpack-deps DSL

---
+++ dsl/docker-multiarch/images/buildpack.groovy
@@ -0,0 +1,68 @@
+def arches = [
+	'arm64',
+	//'armel',
+	//'armhf',
+	//'ppc64le',
+	//'s390x',
+]
+
+freeStyleJob("docker-${arch}-debian") {
+	logRotator { daysToKeep(30) }
+	label("docker-${arch}")
+	scm {
+		git {
+			url('https://github.com/docker-library/buildpack-deps.git')
+			clean()
+		}
+	}
+	triggers {
+		upstream("docker-${arch}-debian, docker-${arch}-ubuntu", 'UNSTABLE')
+		scm('H H/6 * * *')
+	}
+	wrappers { colorizeOutput() }
+	steps {
+		shell("""\
+prefix='${arch}'
+repo='${arch}/buildpack-deps'
+
+sed -i "s!^FROM !FROM \$prefix/!" Dockerfile.template*
+./update.sh
+
+for v in */; do
+	v="\${v%/}"
+	if \\
+		! docker inspect "\$prefix/debian:\$v" &> /dev/null \\
+		&& ! docker inspect "\$prefix/ubuntu:\$v" &> /dev/null \\
+	; then
+		echo >&2 "warning, neither '\$prefix/debian:\$v' or '\$prefix/ubuntu:\$v' exist; skipping \$v "
+		rm -r "\$v/"
+	fi
+done
+
+latest="\$(./generate-stackbrew-library.sh | awk '\$1 == "latest:" { print \$3; exit }')"
+failed=0
+for v in */; do
+	v="\${v%/}"
+	if \\
+		docker build -t "\$repo:\$v-curl" "\$v/curl" \\
+		&& docker build -t "\$repo:\$v-scm" "\$v/scm" \\
+		&& docker build -t "\$repo:\$v" "\$v"; then
+		if [ "\$v" = 'latest' ]; then
+			docker tag -f "\$repo:\$v-curl" "\$repo:curl"
+			docker tag -f "\$repo:\$v-scm" "\$repo:scm"
+			docker tag -f "\$repo:\$v" "\$repo"
+		fi
+	else
+		echo >&2 "Failed to build some part of \$v; moving on."
+		(( failed++ )) || true
+	fi
+done
+
+docker images "\$repo" \\
+	| awk -F '  +' 'NR>1 { print \$1 ":" \$2 }' \\
+	| xargs -rtn1 docker push
+
+exit "\$failed"
+""")
+	}
+}

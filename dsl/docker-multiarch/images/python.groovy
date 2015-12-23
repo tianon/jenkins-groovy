f6db6dbb6eed826a07e36ccdbd862b1d0b06d5be
Tianon Gravi <admwiggin@gmail.com>
2015-12-23 15:50:37 -0800

    Add initial python.groovy

---
+++ dsl/docker-multiarch/images/python.groovy
@@ -0,0 +1,56 @@
+def arches = [
+	'arm64',
+	'armel',
+	'armhf',
+	'ppc64le',
+	's390x',
+]
+
+for (arch in arches) {
+	freeStyleJob("docker-${arch}-python") {
+		logRotator { daysToKeep(30) }
+		label("docker-${arch}")
+		scm {
+			git {
+				remote { url('https://github.com/docker-library/python.git') }
+				branches('*/master')
+				clean()
+			}
+		}
+		triggers {
+			upstream("docker-${arch}-debian, docker-${arch}-buildpack", 'UNSTABLE')
+			scm('H H/6 * * *')
+		}
+		wrappers { colorizeOutput() }
+		steps {
+			shell("""\
+prefix='${arch}'
+repo="\$prefix/python"
+
+sed -i "s!^FROM !FROM \$prefix/!" */{,*/}Dockerfile
+./update.sh
+
+latest="\$(./generate-stackbrew-library.sh | awk '\$1 == "latest:" { print \$3; exit }')"
+
+for v in */; do
+	v="\${v%/}"
+	docker build -t "\$repo:\$v" "\$v"
+	docker build -t "\$repo:\$v-onbuild" "\$v/onbuild"
+	docker build -t "\$repo:\$v-slim" "\$v/slim"
+	if [ "\$v" = 'latest' ]; then
+		docker tag -f "\$repo:\$v" "\$repo"
+		docker tag -f "\$repo:\$v-onbuild" "\$repo:onbuild"
+		docker tag -f "\$repo:\$v-slim" "\$repo:slim"
+	fi
+done
+
+# we don't have /u/arm64
+if [ '${arch}' != 'arm64' ]; then
+	docker images "\$repo" \\
+		| awk -F '  +' 'NR>1 { print \$1 ":" \$2 }' \\
+		| xargs -rtn1 docker push
+fi
+""")
+		}
+	}
+}

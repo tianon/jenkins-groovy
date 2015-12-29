e12a6cad86f11faf7bd22c8f0da277de0d9d18d8
Tianon Gravi <admwiggin@gmail.com>
2015-12-28 16:11:33 -0800

    Add simple initial push.groovy

---
+++ dsl/official-images/docs/push.groovy
@@ -0,0 +1,33 @@
+freeStyleJob('official-images-docs-push') {
+	logRotator { numToKeep(5) }
+	label('infosiftr')
+	scm {
+		git {
+			remote { url('https://github.com/docker-library/docs.git') }
+			branches('*/master')
+			clean()
+		}
+	}
+	triggers {
+		upstream('official-images-docs-update', 'UNSTABLE')
+	}
+	wrappers {
+		colorizeOutput()
+		credentialsBinding {
+			usernamePassword('USERNAME', 'PASSWORD', 'docker-hub-stackbrew')
+		}
+	}
+	steps {
+		shell("""\
+docker build --pull -t docker-library-docs .
+test -t 1 && it='-it' || it='-i'
+set +x
+docker run "\$it" --rm -e TERM \\
+	--entrypoint './push.pl' \\
+	docker-library-docs \\
+	--username "\$USERNAME" \\
+	--password "\$PASSWORD" \\
+	--batchmode */
+""")
+	}
+}

6db85de48e4ce52930ee3ffcf4797ea58d8fd8c9
Tianon Gravi <admwiggin@gmail.com>
2015-12-28 16:56:20 -0800

    Add "diff.groovy"

---
+++ dsl/official-images/diff.groovy
@@ -0,0 +1,26 @@
+freeStyleJob('official-images-diff') {
+	logRotator { daysToKeep(30) }
+	label('infosiftr')
+	scm {
+		git {
+			remote { url('https://github.com/docker-library/official-images.git') }
+			branches('*/master')
+			clean()
+		}
+	}
+	triggers {
+		cron('H H * * *')
+		scm('H/5 * * * *')
+	}
+	wrappers { colorizeOutput() }
+	steps {
+		shell("""\
+diff -u \\
+	<({ echo scratch; ls -1 library; } \\
+		| sort) \\
+	<(curl -fsSL 'https://hub.docker.com/v2/repositories/library/?page_size=1000&page=1' \\
+		| jq --raw-output '.results[].name' \\
+		| sort)
+""")
+	}
+}

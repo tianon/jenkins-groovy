6c48a83019ded96ad102e87ba227e7db915d69be
Tianon Gravi <admwiggin@gmail.com>
2015-12-29 13:46:13 -0800

    Tweak the wording a little bit

--- dsl/docker-multiarch/docs.groovy
+++ dsl/docker-multiarch/docs.groovy
@@ -47,7 +47,7 @@ cat > .template-helpers/generate-dockerfile-links-partial.sh <<-'EOF'
 	echo "This image is built from the source of the [official image of the same name (\\`\$1\\`)](https://hub.docker.com/_/\$1/).  Please see that image's description for links to the relevant \\`Dockerfile\\`s."
 	echo
 	
-	echo 'If you are curious about specifically how these images are built, see [the Jenkins Groovy DSL scripts in the `tianon/jenkins-groovy` GitHub repository](https://github.com/tianon/jenkins-groovy/tree/master/dsl/docker-multiarch/images).'
+	echo 'If you are curious about specifically how this image differs, see [the Jenkins Groovy DSL scripts in the `tianon/jenkins-groovy` GitHub repository](https://github.com/tianon/jenkins-groovy/tree/master/dsl/docker-multiarch/images), which are responsible for creating the Jenkins jobs which build them.'
 	echo
 EOF
 sed -ri "s!^docker pull !#&!; s!^(docker run --rm|docker images) !\\1 \$prefix/!" hello-world/update.sh

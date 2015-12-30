f8f071e0c5af1fd8a5b90e5c7192715a97826f53
Tianon Gravi <admwiggin@gmail.com>
2015-12-30 12:33:06 -0800

    Add initial "issue-labeler" job

---
+++ dsl/official-images/labels.groovy
@@ -0,0 +1,28 @@
+freeStyleJob('official-images-labeler') {
+	logRotator { numToKeep(5) }
+	label('infosiftr')
+	scm {
+		git {
+			remote { url('https://github.com/yosifkit/official-images-issue-labeler.git') }
+			branches('*/master')
+			clean()
+		}
+	}
+	triggers {
+		cron('H/15 * * * *')
+		//scm('H/5 * * * *')
+	}
+	wrappers {
+		colorizeOutput()
+		credentialsBinding {
+			string('GITHUB_TOKEN', 'github-token-docker-library-bot')
+		}
+	}
+	steps {
+		shell("""\
+docker build -t docker-library-issue-labeler .
+set +x
+docker run --rm docker-library-issue-labeler go-wrapper run "\$GITHUB_TOKEN"
+""")
+	}
+}

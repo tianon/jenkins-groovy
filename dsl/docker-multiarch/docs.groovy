acbf549f21c4f065d318c1b74fd9ed5ed3408ace
Tianon Gravi <admwiggin@gmail.com>
2015-12-30 00:10:46 -0800

    Add initial haproxy job

--- dsl/docker-multiarch/docs.groovy
+++ dsl/docker-multiarch/docs.groovy
@@ -13,6 +13,7 @@ def images = [
 	'debian',
 	'erlang',
 	'gcc',
+	'haproxy',
 	'hello-world',
 	'irssi',
 	'perl',
--- dsl/docker-multiarch/views.groovy
+++ dsl/docker-multiarch/views.groovy
@@ -13,6 +13,7 @@ def images = [
 	'debian',
 	'erlang',
 	'gcc',
+	'haproxy',
 	'hello',
 	'irssi',
 	'node',

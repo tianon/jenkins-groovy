609610b2bc2ca4ba7483ffff3a844e49ba6e0e56
Tianon Gravi <admwiggin@gmail.com>
2015-12-30 09:40:04 -0800

    Add initial tianon-syncthing job

---
+++ dsl/tianon/syncthing.groovy
@@ -0,0 +1,58 @@
+freeStyleJob('tianon-jenkins') {
+	logRotator { numToKeep(5) }
+	label('tianon-nameless')
+	scm {
+		git {
+			remote {
+				url('git@github.com:tianon/docker-syncthing.git')
+				credentials('tianon')
+			}
+			branches('*/master')
+			clean()
+		}
+	}
+	triggers {
+		cron('H H/6 * * *')
+		scm('H/5 * * * *')
+	}
+	wrappers { colorizeOutput() }
+	steps {
+		shell("""\
+./update.sh
+
+./build.sh
+./push.sh
+
+docker tag -f tianon/syncthing:cli tianon/syncthing-cli
+docker tag -f tianon/syncthing:inotify tianon/syncthing-inotify
+
+docker push tianon/syncthing-cli
+docker push tianon/syncthing-inotify
+
+for v in */; do
+	v="\${v%/}"
+	full="\$(awk '
+		\$1 == "ENV" && (\$2 == "SYNCTHING_VERSION" || \$2 == "SYNCTHING_INOTIFY_VERSION") {
+			print \$3;
+		}
+	' "\$v/Dockerfile")"
+	case "\$v" in
+		inotify)
+			git commit -m "Update inotify to \$full" -- "\$v/Dockerfile || true
+			;;
+		*)
+			if [ "\$full" ]; then
+				git commit -m "Update to \$full" -- "\$v/Dockerfile" || true
+			fi
+			;;
+	esac
+done
+""")
+	}
+	publishers {
+		git {
+			branch('origin', 'master')
+			pushOnlyIfSuccess()
+		}
+	}
+}

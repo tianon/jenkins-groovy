3006c7c0d9c0f73c61d7921bad7930f933b5228d
Tianon Gravi <admwiggin@gmail.com>
2015-12-30 09:23:53 -0800

    Add initial tianon-jenkins job outline

---
+++ dsl/tianon/jenkins.groovy
@@ -0,0 +1,38 @@
+freeStyleJob('tianon-jenkins') {
+	logRotator { numToKeep(5) }
+	label('tianon-nameless')
+	scm {
+		git {
+			remote {
+				url('git@github.com:tianon/dockerfiles.git')
+				credentials('tianon')
+			}
+			branches('*/master')
+			clean()
+		}
+	}
+	triggers {
+		cron('H H * * *')
+		scm('H/5 * * * *')
+	}
+	wrappers { colorizeOutput() }
+	steps {
+		shell("""\
+cd jenkins
+./update.sh
+docker build --pull -t tianon/jenkins .
+jenkinsVersion="$(awk '
+	\$1 == "ENV" && \$2 == "JENKINS_VERSION" {
+		print \$3;
+	}
+' Dockerfile)"
+git commit -m "Update jenkins to \$jenkinsVersion" -- Dockerfile || true
+""")
+	}
+	publishers {
+		git {
+			branch('origin', 'master')
+			pushOnlyIfSuccess()
+		}
+	}
+}

2925ab8229b9c1906d37a5705d8b69447f6efe9d
Tianon Gravi <admwiggin@gmail.com>
2016-01-03 08:29:35 -0800

    Add job for updating https://github.com/tianon/debian-security-tracker-mirror

---
+++ dsl/tianon/debsec.groovy
@@ -0,0 +1,31 @@
+freeStyleJob('tianon-debian-security-tracker-mirror') {
+	logRotator { daysToKeep(30) }
+	label('tianon')
+	scm {
+		git {
+			remote {
+				url('git@github.com:tianon/debian-security-tracker-mirror.git')
+				credentials('tianon')
+			}
+			branches('*/master')
+			clean()
+		}
+	}
+	triggers {
+		cron('H H * * *')
+	}
+	wrappers { colorizeOutput() }
+	steps {
+		shell("""\
+./update.sh
+
+git commit -m 'Update debian-security.json' -- debian-security.json || true
+""")
+	}
+	publishers {
+		git {
+			branch('origin', 'master')
+			pushOnlyIfSuccess()
+		}
+	}
+}

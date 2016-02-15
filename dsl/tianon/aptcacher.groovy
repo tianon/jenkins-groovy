31eaecae2fa9c055cfa9a1102244e59d5c789e94
Tianon Gravi <admwiggin@gmail.com>
2016-02-15 11:55:57 -0800

    Add a basic apt-cacher-ng "daily cron" job to clean stale data

---
+++ dsl/tianon/aptcacher.groovy
@@ -0,0 +1,21 @@
+matrixJob('tianon-apt-cacher-maint') {
+	logRotator { numToKeep(5) }
+	label('tianon')
+	triggers {
+		cron('H H * * *')
+	}
+	axes {
+		label('host', 'tianon-nameless', 'tianon-viper')
+	}
+	wrappers { colorizeOutput() }
+	steps {
+		shell('''\
+#!/bin/bash
+set -eo pipefail
+
+if docker inspect --type container apt-cacher-ng &> /dev/null; then
+	docker exec apt-cacher-ng /etc/cron.daily/apt-cacher-ng
+fi
+''')
+	}
+}

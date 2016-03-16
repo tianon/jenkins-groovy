72c61af4fd3f6cde1d6b5e4808c4ee8cb94bb550
Vaibhav Sood <vaibhavsood12@gmail.com>
2016-03-16 03:33:44 -0700

    Add new groovy scripts for maven and neo4j

---
+++ dsl/docker-multiarch/images/maven.groovy
@@ -0,0 +1,41 @@
+import vars.multiarch
+
+for (arch in multiarch.allArches()) {
+	meta = multiarch.meta(getClass(), arch)
+
+	freeStyleJob(meta.name) {
+		description(meta.description)
+		logRotator { daysToKeep(30) }
+		label(meta.label)
+		scm {
+			git {
+				remote { url('https://github.com/carlossg/docker-maven.git') }
+				branches('*/master')
+				clean()
+			}
+		}
+		triggers {
+			upstream("docker-${arch}-openjdk", 'UNSTABLE')
+			scm('H H/6 * * *')
+		}
+		wrappers { colorizeOutput() }
+		steps {
+			shell(multiarch.templateArgs(meta) + '''
+rm -rf tests
+sed -i "s!^FROM !FROM $prefix/!" */{*/}Dockerfile
+
+latest="$(./generate-stackbrew-library.sh | awk '$1 == "latest:" { print $3; exit }')"
+
+for v in */; do
+	v="${v%/}"
+	docker build -t "$repo:$v" "$v"
+	docker build -t "$repo:$v-onbuild" "$v/onbuild"
+	if [ "$v" = "$latest" ]; then
+		docker tag -f "$repo:$v" "$repo"
+		docker tag -f "$repo:$v-onbuild" "$repo:onbuild"
+	fi
+done
+''' + multiarch.templatePush(meta))
+		}
+	}
+}

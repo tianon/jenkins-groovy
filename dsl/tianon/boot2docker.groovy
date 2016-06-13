b5e89711119fe27ed7c6d7535877d1ba8ed8a982
Tianon Gravi <admwiggin@gmail.com>
2016-06-13 16:26:30 -0700

    Add "boot2docker-experimental.iso" to all b2d builds

--- dsl/tianon/boot2docker.groovy
+++ dsl/tianon/boot2docker.groovy
@@ -4,7 +4,9 @@ def releaseTypes = [
 		'shell': '''\
 git-set-mtimes
 
-touch "version-GA-$(< VERSION)"
+dockerVersion="$(< VERSION)"
+
+touch "version-GA-$dockerVersion"
 
 docker build -t boot2docker/boot2docker --pull .
 docker run --rm boot2docker/boot2docker > boot2docker.iso
@@ -15,15 +17,15 @@ docker run --rm boot2docker/boot2docker > boot2docker.iso
 		'shell': '''\
 git-set-mtimes
 
-testDocker="$(curl -fsSL 'http://test.docker.com.s3.amazonaws.com/latest')"
-testDockerSha256="$(curl -fsSL "http://test.docker.com.s3.amazonaws.com/builds/Linux/x86_64/docker-${testDocker}.tgz.sha256" | cut -d' ' -f1)"
+dockerVersion="$(curl -fsSL 'http://test.docker.com.s3.amazonaws.com/latest')"
+testDockerSha256="$(curl -fsSL "http://test.docker.com.s3.amazonaws.com/builds/Linux/x86_64/docker-${dockerVersion}.tgz.sha256" | cut -d' ' -f1)"
 
-touch "version-TEST-$testDocker"
+touch "version-TEST-$dockerVersion"
 
 cat > Dockerfile.test <<EOD
 FROM boot2docker/boot2docker
 
-ENV TEST_DOCKER_VERSION ${testDocker}
+ENV TEST_DOCKER_VERSION ${dockerVersion}
 ENV TEST_DOCKER_SHA256 ${testDockerSha256}
 
 RUN set -x \\
@@ -33,10 +35,10 @@ RUN set -x \\
 	&& rm /tmp/dockerbin.tgz \\
 	&& chroot "\\$ROOTFS" docker -v
 
-RUN \\$ROOTFS/usr/local/bin/docker -v | sed -r 's/.* version ([^ ,]+).*/\\1/' > \\$ROOTFS/etc/version \\
-	&& cp -v \\$ROOTFS/etc/version /tmp/iso/version
+RUN chroot "\\$ROOTFS" docker -v | sed -r 's/.* version ([^ ,]+).*/\\1/' > "\\$ROOTFS/etc/version" \\
+	&& cp -v "\\$ROOTFS/etc/version" /tmp/iso/version
 
-RUN { echo; echo "  WARNING: this is a test.docker.com build, not a release."; echo; } >> \\$ROOTFS/etc/motd
+RUN { echo; echo "  WARNING: this is a build from test.docker.com, not a stable release."; echo; } >> "\\$ROOTFS/etc/motd"
 
 RUN /make_iso.sh
 EOD
@@ -47,6 +49,34 @@ docker run --rm boot2docker/boot2docker:test > boot2docker.iso
 ''',
 	],
 ]
+
+def experimentalShell = '''
+experimentalDockerSha256="$(curl -fsSL "http://experimental.docker.com.s3.amazonaws.com/builds/Linux/x86_64/docker-${dockerVersion}.tgz.sha256" | cut -d' ' -f1)"
+
+cat > Dockerfile.experimental <<EOD
+FROM boot2docker/boot2docker
+
+ENV EXPERIMENTAL_DOCKER_VERSION ${dockerVersion}
+ENV EXPERIMENTAL_DOCKER_SHA256 ${experimentalDockerSha256}
+
+RUN set -x \\
+	&& curl -fSL http://experimental.docker.com.s3.amazonaws.com/builds/Linux/x86_64/docker-\\$EXPERIMENTAL_DOCKER_VERSION.tgz -o /tmp/dockerbin.tgz \\
+	&& echo "\\${EXPERIMENTAL_DOCKER_SHA256} */tmp/dockerbin.tgz" | sha256sum -c - \\
+	&& tar -zxvf /tmp/dockerbin.tgz -C "\\$ROOTFS/usr/local/bin" --strip-components=1 \\
+	&& rm /tmp/dockerbin.tgz \\
+	&& chroot "\\$ROOTFS" docker -v
+
+RUN chroot "\\$ROOTFS" docker -v | sed -r 's/.* version ([^ ,]+).*/\\1/' > "\\$ROOTFS/etc/version" \\
+	&& cp -v "\\$ROOTFS/etc/version" /tmp/iso/version
+
+RUN { echo; echo "  WARNING: this is a build from experimental.docker.com, not a stable release."; echo; } >> "\\$ROOTFS/etc/motd"
+
+RUN /make_iso.sh
+EOD
+
+docker build -t boot2docker/boot2docker:experimental -f Dockerfile.experimental .
+docker run --rm boot2docker/boot2docker:experimental > boot2docker-experimental.iso
+'''
 
 for (releaseType in releaseTypes) {
 	freeStyleJob(releaseType.key) {
@@ -69,7 +99,7 @@ for (releaseType in releaseTypes) {
 		}
 		wrappers { colorizeOutput() }
 		steps {
-			shell(releaseType.value['shell'])
+			shell(releaseType.value['shell'] + experimentalShell)
 		}
 		publishers {
 			archiveArtifacts {

619f49031817ed7b82baa3c054f8bf612fd6fd21
Tianon Gravi <admwiggin@gmail.com>
2016-10-01 16:48:45 -0700

    Check for directories existing

--- dsl/docker-multiarch/images/node.groovy
+++ dsl/docker-multiarch/images/node.groovy
@@ -80,19 +80,27 @@ for v in */; do
 		EOF
 		continue
 	fi
+
 	docker build -t "$repo:$v" "$v"
 	docker tag -f "$repo:$v" "$repo:$fullVersion"
-	docker build -t "$repo:$v-onbuild" "$v/onbuild"
-	docker tag -f "$repo:$v-onbuild" "$repo:$fullVersion-onbuild"
-	docker build -t "$repo:$v-slim" "$v/slim"
-	docker tag -f "$repo:$v-slim" "$repo:$fullVersion-slim"
-	docker build -t "$repo:$v-wheezy" "$v/wheezy"
-	docker tag -f "$repo:$v-wheezy" "$repo:$fullVersion-wheezy"
-	if [ "$v" = "$latest" ]; then
-		docker tag -f "$repo:$v" "$repo"
-		docker tag -f "$repo:$v-onbuild" "$repo:onbuild"
-		docker tag -f "$repo:$v-slim" "$repo:slim"
-		docker tag -f "$repo:$v-wheezy" "$repo:wheezy"
+	[ "$v" = "$latest" ] && docker tag -f "$repo:$v" "$repo"
+
+	if [ -d "$v/onbuild" ]; then
+		docker build -t "$repo:$v-onbuild" "$v/onbuild"
+		docker tag -f "$repo:$v-onbuild" "$repo:$fullVersion-onbuild"
+		[ "$v" = "$latest" ] && docker tag -f "$repo:$v-onbuild" "$repo:onbuild"
+	fi
+
+	if [ -d "$v/slim" ]; then
+		docker build -t "$repo:$v-slim" "$v/slim"
+		docker tag -f "$repo:$v-slim" "$repo:$fullVersion-slim"
+		[ "$v" = "$latest" ] && docker tag -f "$repo:$v-slim" "$repo:slim"
+	fi
+
+	if [ -d "$v/wheezy" ]; then
+		docker build -t "$repo:$v-wheezy" "$v/wheezy"
+		docker tag -f "$repo:$v-wheezy" "$repo:$fullVersion-wheezy"
+		[ "$v" = "$latest" ] && docker tag -f "$repo:$v-wheezy" "$repo:wheezy"
 	fi
 done
 ''' + multiarch.templatePush(meta))

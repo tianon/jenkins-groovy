5d78ca096f92ec39ddb152b6a7d5f21b3c872f31
Tianon Gravi <admwiggin@gmail.com>
2017-03-01 13:43:22 -0800

    Add initial "opensuse" job (no triggers for now so I can test the code)

---
+++ dsl/docker-multiarch/images/opensuse.groovy
@@ -0,0 +1,69 @@
+import vars.multiarch
+
+def static opensuseArch(arch) {
+	// see http://download.opensuse.org/repositories/Virtualization:/containers:/images:/openSUSE-Tumbleweed/images/
+	multiarch.optional([
+		'amd64': 'x86_64',
+		'arm64': 'aarch64',
+		'armhf': 'armv7l',
+		'ppc64le': 'ppc64le',
+		's390x': 's390x',
+	], arch, null)
+}
+
+for (arch in multiarch.allArches()) {
+	meta = multiarch.meta(getClass(), arch)
+	meta.opensuseArch = opensuseArch(arch)
+	if (meta.opensuseArch == null) {
+		// skip unsupported architectures
+		continue
+	}
+
+	freeStyleJob(meta.name) {
+		description(meta.description)
+		logRotator { daysToKeep(30) }
+		concurrentBuild(false)
+		label(meta.label)
+		triggers {
+			//cron('H H * * H')
+			//scm('H H/6 * * *')
+		}
+		wrappers { colorizeOutput() }
+		steps {
+			shell(multiarch.templateArgs(meta, ['opensuseArch']) + '''
+sudo rm -rf tmp
+mkdir -p tmp
+
+# explicit list of versions we'll attempt to fetch
+latest='42.2' # should match https://github.com/docker-library/official-images/blob/master/library/opensuse
+versions=(
+	42.2
+	42.1
+	13.2
+	Tumbleweed
+)
+
+for v in "${versions[@]}"; do
+	for urlBase in \\
+		"http://download.opensuse.org/repositories/Virtualization:/containers:/images:/openSUSE-${v}/images" \\
+	; do
+		rm -rf "tmp/$v"; mkdir -p "tmp/$v"
+
+		tarballName="openSUSE-${v}-docker-guest-docker.${opensuseArch}.tar.xz"
+
+		curl -fSL "$urlBase/$tarballName" -o "tmp/$v/rootfs.tar.xz" || continue
+
+		tee "tmp/$v/Dockerfile" <<-'EOD'
+			FROM scratch
+			ADD rootfs.tar.xz /
+		EOD
+		docker build -t "$repo:${v,,}" "tmp/$v"
+		if [ "$v" = "$latest" ]; then
+			docker tag "$repo:${v,,}" "$repo"
+		fi
+	done
+done
+''' + multiarch.templatePush(meta))
+		}
+	}
+}
